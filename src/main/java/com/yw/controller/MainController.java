a4b9d4db828f3ade4db8c0c2d594ba091464fd55
Yingguo <wuyingguo.bit@gmail.com>
2017-05-08 16:37:17 -0400

    Added Logger

--- src/main/java/com/yw/controller/MainController.java
+++ src/main/java/com/yw/controller/MainController.java
@@ -1,15 +1,17 @@
 package com.yw.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-
+final static Logger logger = Logger.getLogger(MainController.class);
   @RequestMapping("/")
   @ResponseBody
   public String index() {
+	  logger.info("Inside Main controller");
     return "Proudly handcrafted by " +
         "<a href='http://yingguowu.com'>YingguoWu</a> :)";
   }
